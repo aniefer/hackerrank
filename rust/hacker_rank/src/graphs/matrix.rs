86dbb00134c45beec6e692b1b155e9193c3f2c69
AndrewNiefer <aniefer@gmail.com>
2021-02-13 22:35:13 -0500

    Use Rc and RefCell for the graph

--- rust/hacker_rank/src/graphs/matrix.rs
+++ rust/hacker_rank/src/graphs/matrix.rs
@@ -4,16 +4,50 @@
 
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::rc::Rc;
+use std::cell::RefCell;
 
-#[derive(Copy, Clone)]
-struct Group {
+
+struct Node {
 	node_id: i32,
 	has_machines: bool,
+	members: Vec<i32>,
 }
 
-impl Group {
+impl Node {
 	fn new(node_id: i32, has_machines: bool) -> Self {
-		Self { node_id, has_machines }
+		Self {
+			node_id,
+			has_machines,
+			members: vec![node_id],
+		}
+	}
+}
+
+struct Graph {
+	nodes : HashMap<i32, Rc<RefCell<Node>>>,
+}
+
+impl Graph {
+	pub fn new() -> Self {
+		Graph { nodes : HashMap::new()}
+	}
+	
+	pub fn get_node(&mut self, node_id:i32, infected: bool) -> Rc<RefCell<Node>> {
+		let node = self.nodes.entry(node_id)
+			.or_insert_with(|| Rc::new(RefCell::new(Node::new(node_id, infected))));
+		node.clone()
+	}
+	
+	pub fn merge(&mut self, rc_node: &Rc<RefCell<Node>>, other: &Rc<RefCell<Node>>) {
+		
+		other.borrow_mut().members.iter().for_each(|idx| {
+			self.nodes.insert(*idx, rc_node.clone());
+		});
+
+		let mut node = rc_node.borrow_mut();
+		node.has_machines |= other.borrow().has_machines;
+		node.members.append(&mut other.borrow_mut().members);
 	}
 }
 
@@ -21,29 +55,25 @@ impl Group {
 pub fn min_time(roads: &mut Vec<[i32; 3]>, machines: Vec<i32>) -> i32 {
 	let infected: HashSet<i32> = machines.into_iter().collect();
 	roads.sort_unstable_by(|a, b| a[2].partial_cmp(&b[2]).unwrap().reverse());
+	
 	let mut time = 0;
-	let mut graph: HashMap<i32, Group> = HashMap::new();
+	let mut graph = Graph::new();
+	
 	for road in roads {
 		let node1: i32 = road[0];
 		let node2: i32 = road[1];
 		let weight: i32 = road[2];
 
-		let g1 = *graph.entry(node1).or_insert_with(|| Group::new(node1, infected.contains(&node1)));
-		let g2 = *graph.entry(node2).or_insert_with(|| Group::new(node2, infected.contains(&node2)));
-
-		if g1.node_id != g2.node_id {
+		let g1 = graph.get_node(node1, infected.contains(&node1));
+		let g2 = graph.get_node(node2, infected.contains(&node2));
+		
+		if g1.borrow().node_id != g2.borrow().node_id {
 			// not the same group
-			if g1.has_machines && g2.has_machines {
-				// both have machines
+			if g1.borrow().has_machines && g2.borrow().has_machines {
+				// both have machines, this road is cut
 				time += weight;
 			} else {
-				// merge the groups
-				for (_, node) in graph.iter_mut() {
-					node.has_machines = g1.has_machines || g2.has_machines;
-					if node.node_id == g2.node_id {
-						node.node_id = g1.node_id;
-					}
-				}
+				graph.merge(&g1, &g2);
 			}
 		}
 	}
